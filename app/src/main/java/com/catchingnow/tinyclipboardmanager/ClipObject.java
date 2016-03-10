1bfa582a258a08dc3a93ff5a88d82c2e73f088db
Right2Drive <bjpfannm@ucalgary.ca>
2016-03-09 21:33:59 -0700

    Updates from weeks ago, added fields to ClipObject

--- app/src/main/java/com/catchingnow/tinyclipboardmanager/ClipObject.java
+++ app/src/main/java/com/catchingnow/tinyclipboardmanager/ClipObject.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 /**
  * Created by heruoxin on 15/1/24.
+ * Altered by MehrunesTenets 02/23/16
  */
 public class ClipObject {
 
@@ -12,16 +13,19 @@ public class ClipObject {
     protected String text;
     protected Date date;
     protected boolean star;
+    protected String name;
 
     public ClipObject(String text, Date date) {
         this.text = text;
         this.date = date;
         this.star = false;
+        this.name = text;
     }
     public ClipObject(String text, Date date, boolean star) {
         this.text = text;
         this.date = date;
         this.star = star;
+        this.name = text;
     }
     public String getText() {
         return text;
@@ -32,6 +36,20 @@ public class ClipObject {
     public  boolean isStarred() {
         return star;
     }
+
+    /**
+     * Return name of ClipObject
+     * Added by MehrunesTenets 02/23/16
+     * @return name of the ClipObject
+     */
+    public String getName() { return name; }
+
+    /**
+     * Set the name of the ClipObject
+     * Added by MehrunesTenets 02/23/16
+     * @param name the new name for the ClipObject
+     */
+    public void setName(String name)   { this.name = name; }
     public ClipObject setStarred(boolean isStarred) {
         this.star = isStarred;
         return this;
